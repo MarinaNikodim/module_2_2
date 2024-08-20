6b8d91ce10940da276c80340e2696e90f89dd9e9
MarinaNikodim <marinenik@yandex.ru>
2024-08-20 18:17:47 +0300

    Add files via upload

---
+++ Module_2_2.py
@@ -0,0 +1,16 @@
+first=123
+second=678
+third=234
+print(first)
+print(second)
+print(third)
+if first==second==third:
+    print(3)
+elif first==second or first==third or second==third:
+    print(2)
+else:
+    print(0)
+
+
+
+
